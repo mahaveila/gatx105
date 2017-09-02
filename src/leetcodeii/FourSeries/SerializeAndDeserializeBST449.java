cad158f2812a8cd02b693a1103a9eed26d061c8f
Youming Ye <Erebus@Youmings-MacBook-Air.local>
2017-09-01 18:59:39 -0700

    update

--- src/leetcodeii/FourSeries/SerializeAndDeserializeBST449.java
+++ src/leetcodeii/FourSeries/SerializeAndDeserializeBST449.java
@@ -3,7 +3,9 @@ package leetcodeii.FourSeries;
 import leetcodeii.Tracker;
 import leetcodeii.TreeNode;
 
+import java.util.Arrays;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -13,6 +15,8 @@ import java.util.function.Function;
  */
 public class SerializeAndDeserializeBST449 implements Tracker{
 
+    private String SEP = ",";
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if(root==null){
@@ -20,16 +24,20 @@ public class SerializeAndDeserializeBST449 implements Tracker{
         }
         StringBuilder sb = new StringBuilder();
 
-        Queue<TreeNode> q = new LinkedBlockingQueue<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode cur = q.poll();
-            sb.append(cur.val);
-            if(cur.left!=null){
-                q.add(cur.left);
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+
+            TreeNode cur = st.pop();
+            if(sb.length()>0){
+                sb.append(SEP);
             }
+            sb.append(""+cur.val);
             if(cur.right!=null){
-                q.add(cur.right);
+                st.push(cur.right);
+            }
+            if(cur.left!=null){
+                st.push(cur.left);
             }
         }
 
@@ -38,35 +46,48 @@ public class SerializeAndDeserializeBST449 implements Tracker{
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        String[] in = data.split(SEP);
+        cout(in);
+        return helper(in);
+    }
+
+    private TreeNode helper(String [] in){
         //return condition
-        if(data==null||data.length()<1){
+        if(in==null||in.length<1){
             return null;
         }
         TreeNode head = new TreeNode(0);
-        head.val = Integer.parseInt(data.substring(0,1));
-        if(data.length()<2){
+        head.val = Integer.parseInt(in[0]);
+        cout("root : " + head.val);
+        if(in.length<2){
             return head;
         }
+
         int right = 0;
-        for(int ii = 1; ii<data.length();ii++){
-            int cur = Integer.parseInt(""+data.charAt(ii));
+        for(int ii = 1; ii<in.length;ii++){
+            int cur = Integer.parseInt(in[ii]);
             if(cur>=head.val){
                 right = ii;
                 break;
             }
         }
+        String[] leftAry =new String[0];
+        String[] rightAry = new String[0];
 
-        if(right>0){
-            //there is right node
-            head.right=deserialize(data.substring(right));
+        if(right==0){
+            leftAry = Arrays.copyOfRange(in, 1, in.length);
+        } else {
+            rightAry = Arrays.copyOfRange(in, right, in.length);
+            if(right>1){
+                leftAry=Arrays.copyOfRange(in, 1, right);
+            }
         }
-        if(right>1){
-            //there is left node before right node
-            head.left=deserialize(data.substring(1,right));
-        } else if(data.length()>1 && right==0){
-            //there are only left nodes
-            head.left=deserialize(data.substring(1));
-        }
+
+        cout("  left child " + stringfy(leftAry));
+        cout("  right child " + stringfy(rightAry));
+
+        head.left=helper(leftAry);
+        head.right=helper(rightAry);
 
         return head;
     }
@@ -77,8 +98,15 @@ public class SerializeAndDeserializeBST449 implements Tracker{
         head.left = new TreeNode(1);
         head.left.right = new TreeNode(2);
         head.right = new TreeNode(4);
-        s.deserialize(s.serialize(head));
+        TreeNode res = s.deserialize(s.serialize(head));
         s.cout(s.serialize(head));
+        s.cout(s.serialize(res));
+
+        TreeNode h2 = new TreeNode(2);
+        h2.left=new TreeNode(1);
+
+        s.cout(s.serialize(h2));
+        s.deserialize(s.serialize(h2));
     }
 
 }
