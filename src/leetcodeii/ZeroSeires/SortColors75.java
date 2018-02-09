d680e6c522fc7b8281a777e1e5db27223fe57110
mahaveila <paulyy0701@gmail.com>
2018-02-09 02:27:36 -0500

    w

---
+++ src/leetcodeii/ZeroSeires/SortColors75.java
@@ -0,0 +1,35 @@
+package leetcodeii.ZeroSeires;
+
+/**
+ * Created by Youming on 2/9/2018.
+ */
+public class SortColors75 {
+
+    //one pass solution
+    public void sortColors(int[] nums) {
+        if(nums==null || nums.length<1){
+            return;
+        }
+        //use one pass
+        int p0 = 0;
+        int p2 = nums.length-1;
+        int i = 0;
+        while(i<=p2){
+            if(nums[i]==0){
+                //find a zero, moves forward, append to the 0s.
+                nums[i]=nums[p0];
+                nums[p0]=0;
+                p0++;
+            }
+            if(nums[i]==2){
+                //find a 2, moves backward, pre-pend to the 2s
+                nums[i]=nums[p2];
+                nums[p2]=2;
+                p2--;
+                i--;
+            }
+            i++;
+        }
+
+    }
+}
